8e2c528e00676263576f2e74fe8e0b47cf6fc62e
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2022-11-05 14:29:13 +0200

    Web API

---
+++ Web API/MyFirstWebAPI/MyFirstWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [Route("/home")]
+    public class HomeController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web API/MyFirstWebAPI/MyFirstWebAPI/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyJSPolicy", policy =>
+    {
+        policy.WithOrigins("htpps..../todo")
+        .AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseCors("MyJSPolicy");
+app.MapControllers();
+app.MapDefaultControllerRoute();
+app.Run();
